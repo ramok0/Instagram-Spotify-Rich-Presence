9ff0d8556968b623fa697b263fd3a7980f5f044f
Ramok <prout@zemmour.xyz>
2022-05-27 17:48:23 +0200

    🚑️ Fixed a critical bug (crash)

--- Config.h
+++ Config.h
@@ -59,33 +59,34 @@ public:
 		nlohmann::json j;
 		if (file.is_open()) {
 			file >> j; //read file
-		}
-		file.close(); //close the stream as the file has been parsed
+			file.close(); //close the stream as the file has been parsed
+			
 
-		std::vector<std::string> keys = { "spotify_client_id", "spotify_client_secret", "spotify_refresh_token", "spotify_access_token", "insta_username", "insta_password","insta_username", "insta_bio", "noColors" };
-		for (auto& key : keys) {
-			if (!utils.jsonExists(j, key)) { //check that every keys exists
+			std::vector<std::string> keys = { "spotify_client_id", "spotify_client_secret", "spotify_refresh_token", "spotify_access_token", "insta_username", "insta_password","insta_username", "insta_bio", "noColors" };
+			for (auto& key : keys) {
+				if (!utils.jsonExists(j, key)) { //check that every keys exists
 #ifdef DEBUG
-				printf("Key %s does not exists!\n", key.c_str());
+					printf("Key %s does not exists!\n", key.c_str());
 #else
-				std::cout << termcolor::bright_red << "Key " << key << " not found in " << this->fileName << ", please check that it exists !" << termcolor::reset << std::endl;
+					std::cout << termcolor::bright_red << "Key " << key << " not found in " << this->fileName << ", please check that it exists !" << termcolor::reset << std::endl;
 #endif	
-				exit(0);
+					exit(0);
+				}
 			}
+			//SPOTIFY REQUIRED
+			this->spotify_client_id = j["spotify_client_id"].get<std::string>();
+			this->spotify_client_secret = j["spotify_client_secret"].get<std::string>();
+			this->spotify_refresh_token = j["spotify_refresh_token"].get<std::string>();
+			this->spotify_access_token = j["spotify_access_token"].get<std::string>();
+
+			//INSTAGRAM REQUIRED
+			this->insta_username = j["insta_username"].get<std::string>();
+			this->insta_password = j["insta_password"].get<std::string>();
+			this->insta_bio = j["insta_bio"].get<std::string>();
+
+			//OPTIONS
+			this->noColors = j["noColors"].get<bool>();
 		}
-		//SPOTIFY REQUIRED
-		this->spotify_client_id = std::string(j["spotify_client_id"]);
-		this->spotify_client_secret = std::string(j["spotify_client_secret"]);
-		this->spotify_refresh_token = std::string(j["spotify_refresh_token"]);
-		this->spotify_access_token = std::string(j["spotify_access_token"]);
-
-		//INSTAGRAM REQUIRED
-		this->insta_username = std::string(j["insta_username"]);
-		this->insta_password = std::string(j["insta_password"]);
-		this->insta_bio = std::string(j["insta_bio"]);
-
-		//OPTIONS
-		this->noColors = j["noColors"].get<bool>();
 	}
 
 	void save() {
